d2db95278a8a41d037eb25e832901e93e7586fa7
KDharshini-1705 <dharshinikupps117@gmail.com>
2025-05-14 21:41:05 +0530

    Add files via upload

---
+++ RAMPEX3/average.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d,e;
+    a=23;
+    b=34;
+    c=98;
+    d=76;
+    e=56;
+    int total=a+b+c+d+e;
+    printf("total=%d\n",total);
+    int average=total/5;
+    printf("average=%d\n",average);
+
+    return 0;
+}
