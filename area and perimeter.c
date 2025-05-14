9c60d1863015564a0d1879204a541c69f6a94db9
KDharshini-1705 <dharshinikupps117@gmail.com>
2025-05-14 11:21:26 +0530

    Add files via upload

---
+++ area and perimeter.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int l,b;
+    l=2;
+    b=4;
+    l*b;
+    2*(l+b);
+    printf("area of a rectangle:%d\n",l*b);
+    printf("perimeter of a rectangle:%d\n",2*(l+b));
+    return 0;
+    }
+    
